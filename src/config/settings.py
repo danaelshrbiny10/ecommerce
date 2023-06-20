df30b33b0c0a1ebb0bfc5449cd8e10a745134dde
Dana <pythondjango66@gmail.com>
2023-06-20 18:12:01 +0300

    update requirements

--- src/config/settings.py
+++ src/config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(^y_g6%n^mt9jav!*ok&l50c8c$7zrw&1)&#wgwpw%35mn-)p1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8000","http://127.0.0.1:8000","http://15.188.207.101:80"]
